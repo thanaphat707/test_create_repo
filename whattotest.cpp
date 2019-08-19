b7c8e107a66f9c8b34cba14b05188a768036caa3
makomawin <mako.meow.meow@gmail.com>
2019-08-19 13:36:11 +0700

    Initial test code
    Add test code to test repro

---
+++ whattotest.cpp
@@ -0,0 +1,18 @@
+// whattotest.cpp
+#include <math.h>
+#include<iostream>
+double b;
+
+double squareRoot(const double a) {
+    //double b = sqrt(a);
+    if(1 != b) { // nan check
+        return -1.0;
+    }else{
+        return sqrt(a);
+    }
+}
+int main2(void)
+{
+    std::cout << "Yeah";
+    return 0;
+}
